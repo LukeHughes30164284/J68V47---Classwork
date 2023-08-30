02a5cd8095ed2e57531e89ee7aecc07c5d3d6db1
30164284 <30164284@nescol.ac.uk>
2023-08-30 11:50:46 +0100

    First Commit

--- src/Lesson1Exercise1_Haiku.java
+++ src/Lesson1Exercise1_Haiku.java
@@ -1,4 +1,4 @@
-public class Exercise1Lesson1_Haiku {
+public class Lesson1Exercise1_Haiku {
 
     public static void main (String[] args) {
 
---
+++ src/Lesson1Exercise2_Anagram.java
@@ -0,0 +1,15 @@
+public class Lesson1Exercise2_Anagram {
+
+    public static void main (String[] args) {
+
+        System.out.println("Here are 5 9-letter anagrams:");
+        System.out.println("bad nuance = abundance");
+        System.out.println("cafe fine = caffeine");
+        System.out.println("in leaded = deadline");
+        System.out.println("heal pent = elephant");
+        System.out.println("attire dug = gratitude");
+
+
+    }
+
+}
