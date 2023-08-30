3ac830be3e31152d713102b88da80465fbcac683
30164284 <30164284@nescol.ac.uk>
2023-08-30 11:42:29 +0100

    First Commit

---
+++ src/Exercise1Lesson1_Haiku.java
@@ -0,0 +1,11 @@
+public class Exercise1Lesson1_Haiku {
+
+    public static void main (String[] args) {
+
+        System.out.println("Rainforest ablaze,");
+        System.out.println("Nature's cry in smoky haze,");
+        System.out.println("World's heart feels the craze.");
+
+    }
+
+}
