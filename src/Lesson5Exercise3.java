7306ee5b3faf415cbfb308a88dcfd4fa2b7364f5
30164284 <30164284@nescol.ac.uk>
2023-09-27 11:11:29 +0100

    Lesson 5

--- src/Lesson5Exercise3.java
+++ src/Lesson5Exercise3.java
@@ -1,16 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 public class Lesson5Exercise3 {
 
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in);
+        int score = 0;
+        for (int count=1; count<=10; count++){
 
-        for( int count=1; count<=10; count++ )
-        {
+            System.out.println("What is 5 + 7?");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter the answer: ");
+            int answer = scanner.nextInt();
 
-
+            if (answer == 12){
+                System.out.println("Well done!");
+                score++;
+            } else {
+                System.out.println("Sorry, the answer is 12");
         }
-
-
     }
 }
