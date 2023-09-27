7e82e7caed7cf2b03aefef4873e568ec3f77f0f0
30164284 <30164284@nescol.ac.uk>
2023-09-27 11:49:54 +0100

    Lesson 5

--- src/Lesson5Exercise3.java
+++ src/Lesson5Exercise3.java
@@ -6,17 +6,42 @@ public class Lesson5Exercise3 {
         int score = 0;
         for (int count=1; count<=10; count++){
 
-            System.out.println("What is 5 + 7?");
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter the answer: ");
-            int answer = scanner.nextInt();
-
-            if (answer == 12){
-                System.out.println("Well done!");
-                score++;
-            } else {
-                System.out.println("Sorry, the answer is 12");
+            if (count == 1 ) {
+                System.out.println("What is 5 + 7?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 12) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 12");
+                }
             }
+            if (count == 2) {
+                System.out.println("What is 5 x 3?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 15) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 15");
+                }
+            }
+            if (count == 3) {
+                System.out.println("What is 5 x 3?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 15) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 12");
+                }
         }
+        System.out.println("Your score is " + score);
     }
 }
