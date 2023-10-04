ce13430f46adf0327dc52f86a799faad2f97f9fd
30164284 <30164284@nescol.ac.uk>
2023-10-04 09:25:07 +0100

    Lesson 6

--- src/Lesson5Exercise3.java
+++ src/Lesson5Exercise3.java
@@ -42,6 +42,90 @@ public class Lesson5Exercise3 {
                     System.out.println("Sorry, the answer is 18");
                 }
             }
+            if (count == 4) {
+                System.out.println("What is 23 + 14?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 47) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 47");
+                }
+            }
+            if (count == 5) {
+                System.out.println("What is 4 x 9?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 36) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 36");
+                }
+            }
+            if (count == 6) {
+                System.out.println("What is 27 / 3?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 9) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 9");
+                }
+            }
+            if (count == 7) {
+                System.out.println("What is 15 + 12?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 27) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 27");
+                }
+            }
+            if (count == 8) {
+                System.out.println("What is 9 x 11?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 99) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 99");
+                }
+            }
+            if (count == 9) {
+                System.out.println("What is 78 - 23?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 55) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 55");
+                }
+            }
+            if (count == 10) {
+                System.out.println("What is 3 x 15?");
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter the answer: ");
+                int answer = scanner.nextInt();
+                if (answer == 45) {
+                    System.out.println("Well done!");
+                    score++;
+                } else {
+                    System.out.println("Sorry, the answer is 45");
+                }
+            }
             System.out.println("Your score is " + score);
         }
     }
---
+++ src/Lesson6Exercise2.java
@@ -0,0 +1,6 @@
+public class Lesson6Exercise2 {
+    public static void main(String[] args) {
+
+
+    }
+}
