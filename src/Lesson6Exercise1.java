43547da12b6878a0ff2f34163397301786834cbd
30164284 <30164284@nescol.ac.uk>
2023-10-04 09:18:34 +0100

    Lesson 6

---
+++ src/Lesson6Exercise1.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Lesson6Exercise1 {
+    public static void main(String[] args) {
+
+        int Count = 10;
+
+        while( Count<=24 ) {
+
+            System.out.println(Count);
+            Count += 2;
+        }
+
+    }
+}
